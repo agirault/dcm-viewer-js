62b26e065cdc3bba6e126774086eb1a396315b49
Alexis Girault <alexis.girault@kitware.com>
2021-09-30 04:30:50 -0400

    Read DICOM with itk.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-// TODO
+import readImageDICOMFileSeries from 'itk/readImageDICOMFileSeries';
+
+// Files
+const fileInput = document.querySelector('input');
+fileInput.addEventListener('change', async (event) => {
+
+  // Get files
+  const { dataTransfer } = event;
+  const files = event.target.files || dataTransfer.files;
+
+  // Read series
+  const itkReader = await readImageDICOMFileSeries(files);
+  window.volumeData = itkReader.image;
+});
--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,9 @@ const path = require('path');
 
 const entry = path.join(__dirname, 'src', 'index.js');
 const html = path.join(__dirname, 'src', 'index.html');
+const itkWebWorkers = path.join('itk', 'WebWorkers', 'ImageIO.worker.js');
+const itkImageIOs = path.join('itk', 'ImageIOs');
+const nodeModules = path.join(__dirname, 'node_modules');
 const dist = path.join(__dirname, 'dist');
 
 module.exports = {
@@ -17,6 +20,18 @@ module.exports = {
         {
           from: html,
           to: dist,
+        },
+        {
+          from: path.join(nodeModules, itkWebWorkers),
+          to: path.join(dist, itkWebWorkers),
+        },
+        {
+          from: path.join(nodeModules, itkImageIOs, '*DICOM*'),
+          to: path.join(dist, itkImageIOs, '[name][ext]'),
+        },
+        {
+          from: path.join(nodeModules, itkImageIOs, '*GDCM*'),
+          to: path.join(dist, itkImageIOs, '[name][ext]'),
         },
       ],
     }),
